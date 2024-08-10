9778e6ce9784534c3847b5dbd97abb6d92cf2119
AniketDhede <125856828+AniketDhede@users.noreply.github.com>
2024-08-10 15:41:58 +0530

    data.sql

---
+++ data.sql
@@ -0,0 +1,8 @@
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1002,'Anish','Learn Prompt',CURRENT_DATE(),false);
+
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1003,'Aniket','Full Stack Java',CURRENT_DATE(),false);
+
+insert into todo(ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(1001,'Anay','Blockchain',CURRENT_DATE(),false);
