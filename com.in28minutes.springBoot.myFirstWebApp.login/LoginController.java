3f245a4094cafd61d22c906237bbe3b6d8f013f6
AniketDhede <125856828+AniketDhede@users.noreply.github.com>
2024-08-10 15:24:02 +0530

    Create LoginController.java

---
+++ com.in28minutes.springBoot.myFirstWebApp.login/LoginController.java
@@ -0,0 +1,29 @@
+ackage com.in28minutes.springBoot.myFirstWebApp.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class LoginController {
+	
+   private Logger logger=LoggerFactory.getLogger(getClass());
+    
+   @RequestMapping("login")
+   public String goToLoginPage(@RequestParam String name,ModelMap m) {
+	   //System.out.println("Hii..."+name);
+		
+		  logger.debug("Request param is {}",name);
+		  
+		  logger.info("Info level"); logger.warn("warn level");
+		  System.out.println("Hiiiii....");
+		 
+		 
+	   m.put("naam", name); 
+	   return "login";
+   } 
+} 
+ 
